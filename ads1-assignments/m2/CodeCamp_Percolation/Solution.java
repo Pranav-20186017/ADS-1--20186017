3f75a0cf14c6be463383e592a0225b8420ebc4d3
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-25 17:39:17 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ads1-assignments/m2/CodeCamp_Percolation/Solution.java
+++ ads1-assignments/m2/CodeCamp_Percolation/Solution.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-    Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
         //unused.
     }
     /**
@@ -11,12 +14,12 @@ final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int size = Integer.parseInt(s.nextLine());
         Percolation p = new Percolation(size);
-        while(s.hasNext()) {
-            p.open(s.nextInt(),s.nextInt());
+        while (s.hasNext()) {
+            p.open(s.nextInt(), s.nextInt());
         }
         System.out.println(p.percolates());
     }
