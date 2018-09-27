42c6ce607d5e48de300822b1821c9c86f833c020
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-27 22:08:27 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ads1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ads1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -5,109 +5,69 @@ public class Solution
     { 
         int top=-1; 
         char items[] = new char[1000]; 
-  
         void push(char x)  
         { 
-            if (top == 999)  
-            { 
+            if (top == 999) { 
                 System.out.println("Stack full"); 
             }  
-            else 
-            { 
+            else { 
                 items[++top] = x; 
             } 
         } 
-  
-        char pop()  
-        { 
-            if (top == -1)  
-            { 
+        char pop() { 
+            if (top == -1) { 
                 System.out.println("Underflow error"); 
                 return '\0'; 
-            }  
-            else 
-            { 
+            } else { 
                 char element = items[top]; 
                 top--; 
                 return element; 
             } 
-        } 
-  
-        boolean isEmpty()  
-        { 
+        }
+        boolean isEmpty() { 
             return (top == -1) ? true : false; 
         } 
     } 
-      
-    /* Returns true if character1 and character2 
-       are matching left and right Parenthesis */
     static boolean isMatchingPair(char character1, char character2) 
     { 
-       if (character1 == '(' && character2 == ')') 
+       if (character1 == '(' && character2 == ')') {
          return true; 
-       else if (character1 == '{' && character2 == '}') 
+       }
+       else if (character1 == '{' && character2 == '}') {
          return true; 
-       else if (character1 == '[' && character2 == ']') 
+       }
+       else if (character1 == '[' && character2 == ']') {
          return true; 
-       else
+       }
+       else {
          return false; 
+       }
     } 
-      
-    /* Return true if expression has balanced  
-       Parenthesis */
     static boolean areParenthesisBalanced(char exp[]) 
     { 
-       /* Declare an empty character stack */
        stack st=new stack(); 
-       
-       /* Traverse the given expression to  
-          check matching parenthesis */
        for(int i=0;i<exp.length;i++) 
        { 
-            
-          /*If the exp[i] is a starting  
-            parenthesis then push it*/
-          if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[') 
+          if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[') {
             st.push(exp[i]); 
-       
-          /* If exp[i] is an ending parenthesis  
-             then pop from stack and check if the  
-             popped parenthesis is a matching pair*/
-          if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']') 
-          { 
-                   
-              /* If we see an ending parenthesis without  
-                 a pair then return false*/
-             if (st.isEmpty()) 
-               { 
+          }
+          if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']') { 
+             if (st.isEmpty()) { 
                    return false; 
-               }  
-       
-             /* Pop the top element from stack, if  
-                it is not a pair parenthesis of character  
-                then there is a mismatch. This happens for  
-                expressions like {(}) */
-             else if ( !isMatchingPair(st.pop(), exp[i]) ) 
-               { 
+               } else if (!isMatchingPair(st.pop(), exp[i]) ) { 
                    return false; 
                } 
           } 
             
        } 
-          
-       /* If there is something left in expression  
-          then there is a starting parenthesis without  
-          a closing parenthesis */
-        
-       if (st.isEmpty()) 
-         return true; /*balanced*/
-       else
-         {   /*not balanced*/
+       if (st.isEmpty()) {
+         return true;
+       }
+       else {
              return false; 
          }  
     }  
-    public static void main(String[] args)  
-    { 
+    public static void main(final String[] args)  { 
         Scanner s = new Scanner(System.in);
         int n = Integer.parseInt(s.nextLine());
         char[] exp;
