d64bd97f7207bbd807b2496b89a0d52c1fd84643
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-27 22:16:50 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ads1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ ads1-assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -20,13 +20,13 @@ final class Solution {
     static boolean isMatchingPair(final char character1,
     final char character2) {
        if (character1 == '(' && character2 == ')') {
-         return true; 
+         return true;
        } else if (character1 == '{' && character2 == '}') {
-         return true; 
+         return true;
        } else if (character1 == '[' && character2 == ']') {
-         return true; 
+         return true;
        } else {
-         return false; 
+         return false;
        }
     }
     /**
