f6bcd758386d6251e6e51f4f890fd460c28064ed
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-29 12:13:40 +0530

    "check_style_score -> 6 of 12 passed. style score: 0/1 "

---
+++ ads1-assignments/m6/Assignment-1/LinkedList.java
@@ -0,0 +1,47 @@
+class LinkedList {
+	Node first = null;
+	int size = 0;
+	class Node {
+		int data;
+		Node next;
+		Node(int inputdata) {
+			this.data = inputdata;
+		}
+	}
+	boolean isEmpty() {
+		return size == 0;
+	}
+	void push(int item) {
+		Node newnode = new Node(item);
+		newnode.next = first;
+		first = newnode;
+		size++;
+	}
+	int newpop(int iteratorval) {
+		Node test = first;
+		int counter = 0;
+		while (counter < iteratorval - 1) {
+			test = test.next;
+			counter++;
+		}
+		int value = test.data;
+		size--;
+		return value;
+	}
+	int pop() {
+		int output = first.data;
+		first = first.next;
+		size--;
+		return output;
+	}
+	String print() {
+		Node test = first;
+		String num = "";
+		while (test != null) {
+			num = num + test.data;
+			test = test.next;
+		}
+		return num;
+	}
+
+}
