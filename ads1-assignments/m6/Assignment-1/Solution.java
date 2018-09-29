ccd0ccaf9c7882dc34d5bcb48bf320ba54f5feb0
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-29 12:26:24 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

--- ads1-assignments/m6/Assignment-1/Solution.java
+++ ads1-assignments/m6/Assignment-1/Solution.java
@@ -7,18 +7,23 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String p = sc.nextLine();
         String q = sc.nextLine();
-        switch(input){
+        switch (input) {
             case "numberToDigits":
                 LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
                 LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
@@ -32,6 +37,7 @@ final class Solution {
                 LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
                 System.out.println(AddLargeNumbers.digitsToNumber(result));
                 break;
+            default: 
         }
     }
     
