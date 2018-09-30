99d63f5fce1f1596e061a34dc0e23ea3d3b238a1
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-09-30 18:58:34 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ads1-assignments/m5/5.1 Deque/Solution.java
+++ ads1-assignments/m5/5.1 Deque/Solution.java
@@ -1,133 +1,133 @@
-import java.util.*;
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Deque<Integer> q = new Deque<>();
-		while (scan.hasNext()) {
-			String[] line = scan.nextLine().split(" ");
-			switch (line[0]) {
-			case "pushLeft":
-				q.pushLeft(Integer.parseInt(line[1]));
-				break;
-			case "pushRight":
-				q.pushRight(Integer.parseInt(line[1]));
-				break;
-			case "popRight":
-				q.popRight();
-				break;
-			case "popLeft":
-				q.popLeft();
-				break;
-			case "size":
-				System.out.println(q.getSize());
-				break;
-			default :
-			}
-		}
-
-	}
-}
+import java.util.Scanner;
 class Deque<Item> {
-	Node first = null;
-	Node last = null;
-	int size = 0;
-	class Node {
-		Item data;
-		Node next;
-		Node(Item data) {
-			this.data = data;
-		}
-	}
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	public int getSize() {
-		return size;
-	}
-	public void pushRight(Item element) {
-		Node oldleft = last;
-		last = new Node(element);
-		if (isEmpty()) {
-			first = last;
-			last.next = null;
-			size++;
-			printList();
-			return;
-		}
-		last.next = null;
-		oldleft.next = last;
-		size++;
-		printList();
-	}
-	public void pushLeft(Item element) {
-		Node oldright = first;
-		first = new Node(element);
-		if (isEmpty()) {
-			last = first;
+    Node first = null;
+    Node last = null;
+    int size = 0;
+    class Node {
+        Item data;
+        Node next;
+        Node(Item data) {
+            this.data = data;
+        }
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public int getSize() {
+        return size;
+    }
+    public void pushRight(Item element) {
+        Node oldleft = last;
+        last = new Node(element);
+        if (isEmpty()) {
+            first = last;
+            last.next = null;
+            size++;
+            printList();
+            return;
+        }
+        last.next = null;
+        oldleft.next = last;
+        size++;
+        printList();
+    }
+    public void pushLeft(Item element) {
+        Node oldright = first;
+        first = new Node(element);
+        if (isEmpty()) {
+            last = first;
 
-			size++;
-			printList();
-			return;
+            size++;
+            printList();
+            return;
 
-		}
-		first.next = oldright;
-		size++;
-		printList();
-	}
-	public Item popLeft() {
-		if (size == 0) {
-			System.out.println("Deck is empty");
-			return null;
-		}
-		Item d = first.data;
-		if (isEmpty()) {
-			return null;
-		}
-		first = first.next;
-		size--;
-		printList();
-		return d;
+        }
+        first.next = oldright;
+        size++;
+        printList();
+    }
+    public Item popLeft() {
+        if (size == 0) {
+            System.out.println("Deck is empty");
+            return null;
+        }
+        Item d = first.data;
+        if (isEmpty()) {
+            return null;
+        }
+        first = first.next;
+        size--;
+        printList();
+        return d;
 
-	}
-	public Item popRight() {
-		if (size == 0) {
-			System.out.println("Deck is empty");
-			return null;
-		}
-		Item d  =  last.data;
-		if (isEmpty()) {
-			return null;
-		}
-		int s = 0;
-		Node get = first;
+    }
+    public Item popRight() {
+        if (size == 0) {
+            System.out.println("Deck is empty");
+            return null;
+        }
+        Item d  =  last.data;
+        if (isEmpty()) {
+            return null;
+        }
+        int s = 0;
+        Node get = first;
 
-		while (s < size - 2) {
-			get = get.next;
-			s++;
-		}
-		last = get;
-		size--;
-		printList();
-		return d;
-	}
-	public void printList() {
-		if (size == 0) {
-			System.out.println("[]");
-			return;
-		}
-		Node tnode = first;
-		String s = "";
-		while (tnode != null) {
-			s += tnode.data + ",";
-			tnode = tnode.next;
-		}
-		String[] str = s.split(",");
-		String pr = "[";
-		for(int i = 0;i<size-1;i++) {
-			pr += str[i] + ", ";
-		}
-		pr += str[size-1] + "]";
-		System.out.println(pr);
-	}
+        while (s < size - 2) {
+            get = get.next;
+            s++;
+        }
+        last = get;
+        size--;
+        printList();
+        return d;
+    }
+    public void printList() {
+        if (size == 0) {
+            System.out.println("[]");
+            return;
+        }
+        Node tnode = first;
+        String s = "";
+        while (tnode != null) {
+            s += tnode.data + ",";
+            tnode = tnode.next;
+        }
+        String[] str = s.split(",");
+        String pr = "[";
+        for(int i = 0;i<size-1;i++) {
+            pr += str[i] + ", ";
+        }
+        pr += str[size-1] + "]";
+        System.out.println(pr);
+    }
 
+}
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Deque<Integer> q = new Deque<>();
+        while (scan.hasNext()) {
+            String[] line = scan.nextLine().split(" ");
+            switch (line[0]) {
+            case "pushLeft":
+                q.pushLeft(Integer.parseInt(line[1]));
+                break;
+            case "pushRight":
+                q.pushRight(Integer.parseInt(line[1]));
+                break;
+            case "popRight":
+                q.popRight();
+                break;
+            case "popLeft":
+                q.popLeft();
+                break;
+            case "size":
+                System.out.println(q.getSize());
+                break;
+            default :
+            }
+        }
+
+    }
 }
