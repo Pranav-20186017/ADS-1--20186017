0e7fee46f2c2257cc06149405abb7c08ab414749
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-01 11:09:53 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- ads1-assignments/m7/7.1 Steque/Solution.java
+++ ads1-assignments/m7/7.1 Steque/Solution.java
@@ -3,6 +3,9 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int n = Integer.parseInt(s.nextLine());
-		System.out.println(n);
+		String[] tokens;
+		for (int i = 0; i < n; i++) {
+			System.out.println(s.nextLine());
+		}
 	}
 }
