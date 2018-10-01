1c6af2d87b782adc6d32715c51c638548dc0c2d7
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-01 11:04:01 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ads1-assignments/m7/7.1 Steque/Solution.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = Integer.parseInt(s.nextLine());
+		System.out.println(n);
+	}
+}
