6618d68197ec6cf53f96864fb877b6d879002aa8
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-01 13:57:33 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ads1-assignments/m7/7.1 Steque/LinkedList.java
+++ ads1-assignments/m7/7.1 Steque/LinkedList.java
@@ -12,18 +12,40 @@ class LinkedList {
      * Class for node.
      */
     class Node {
+    	/**
+    	 * variable to hold data.
+    	 */
         private int data;
+        /**
+         * variable to point towards the next Link.
+         */
         private Node next;
     }
+    /**
+     * variable that is at the start of the Linked List.
+     */
     private Node head;
+    /**
+     * variable that is at the end of the Linked List.
+     */
     private Node tail;
+    /**
+     * varibale to measure the size of the Linked List
+     */
     private int size = 0;
     /**
-     * { function_description }
-     *
-     * @param      val   The value
+     * This method pushes an element i.e creates a new head in the linked list.
+     * This mehtod pertains to the stack portion of the Stqueue.
+     * Thus it follows the LIFO logic.
+     * Since insertions are done purely based on creating a new head,
+     * the Time Complexity  O(1), this is also because in an unordered linked list
+     * insertions take O(1) time irrespective of the size.
+     * @param      val {is of type integer}.
      */
     public void push(final int val) {
+    	/**
+    	 * create a new node in the Linked List.
+    	 */
         Node node = new Node();
         node.data = val;
         node.next = null;
@@ -38,11 +60,18 @@ class LinkedList {
         size++;
     }
     /**
-     * { function_description }
-     *
-     * @param      val   The value
+     * This method enqueues an element i.e. adds the new element at the end of the stqueue
+     * Therefore its ultimatley creating a new tail.
+     * Thus it follows the FIFO Logic.
+     * Since insertions are done purely based on creating a new tail,
+     * the Time Complexity O(1), this is also because in an unordered linked lsit
+     * insertions take O(1) time irrespective of the size.
+     * @param      val   {is of type integer}
      */
     public void enqueue(final int val) {
+    	/**
+    	 * create a new node in the Linked List.
+    	 */
         Node node = new Node();
         node.data = val;
         node.next = null;
@@ -61,6 +90,7 @@ class LinkedList {
      * Time Complexity O(1) - Constant Time.
      * The time taken to severe the head is always constant irrespective of the size
      * of the linked list that is used to implement the stequeue.
+     * this method employs the LIFO logic.
      */
     public void pop() {
         head = head.next;
