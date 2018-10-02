610fa09ac6440b30be3676f6a629aa72876acada
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-03 00:58:57 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ads1-assignments/m8/8.2 Insertion sort with sentinel/Solution.java
+++ ads1-assignments/m8/8.2 Insertion sort with sentinel/Solution.java
@@ -1,18 +1,71 @@
-import java.util.*;
-class Insertion {
-	void sort() {
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-}
-class Solution {
-	public static void main(String[] args) {
-		Scanner s  = new Scanner(System.in);
-		int n = s.nextInt();
-		// System.out.println(n);
-		for (int i = 0; i <= n; i++) {
-			String line = s.nextLine();
-			String[] tokens = line.split(" ");
-			System.out.println(Arrays.toString(tokens));
-		}
-	}
+    }
+    /**
+     * Array list containing the cricket objects.
+     */
+    private static ArrayList<Cricket> cricketList
+        = new ArrayList<Cricket>();
+    /**
+     * main function to read input.
+     * Time Complexity : O(N) (while loop).
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        final int three = 3;
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(",");
+            Cricket cricket = new Cricket(tokens[0],
+                                          Integer.parseInt(tokens[1]),
+                                          Integer.parseInt(tokens[2]),
+                                          Integer.parseInt(tokens[three]));
+            cricketList.add(cricket);
+        }
+
+        insertionSort();
+
+        //print ArrayList
+        for (int i = 0; i < cricketList.size() - 1; i++) {
+            System.out.print(cricketList.get(i).name + ",");
+        }
+        System.out.print(cricketList.get(
+                             cricketList.size() - 1).name);
+        System.out.println();
+
+    }
+    /**
+     * method to perform insertion sort
+     * Time Complexities .
+     * Best - O(N)
+     * Worst - O(N^2)
+     * Average - O(N^2)
+     *
+     */
+    public static void insertionSort() {
+        for (int i = cricketList.size() - 1; i >= 0; i--) {
+            Cricket elem = cricketList.get(i);
+            for (int j = i + 1; j <= cricketList.size() - 1; j++) {
+                if (elem.compareTo(cricketList.get(j)) == -1) {
+                    Cricket temp = cricketList.get(j);
+                    cricketList.set(j, elem);
+                    cricketList.set(j - 1, temp);
+                }
+            }
+
+        }
+
+    }
+
 }
