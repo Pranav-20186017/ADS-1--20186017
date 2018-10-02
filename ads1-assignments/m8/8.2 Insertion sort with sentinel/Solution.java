8ed63269e986281b0c120b560cca0419b16ac0bc
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-02 10:54:02 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ads1-assignments/m8/8.2 Insertion sort with sentinel/Solution.java
+++ ads1-assignments/m8/8.2 Insertion sort with sentinel/Solution.java
@@ -8,6 +8,11 @@ class Solution {
 	public static void main(String[] args) {
 		Scanner s  = new Scanner(System.in);
 		int n = s.nextInt();
-		System.out.println(n);
+		// System.out.println(n);
+		for (int i = 0; i < n; i++) {
+			String line = s.nextLine();
+			String[] tokens = line.split(" ");
+			System.out.println(Arrays.toString(tokens));
+		}
 	}
 }
