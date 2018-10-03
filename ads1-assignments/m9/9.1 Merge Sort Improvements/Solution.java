6465d8f006c8cd15d433a92feca2fa0f68d1e636
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-03 23:10:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ads1-assignments/m9/9.1 Merge Sort Improvements/Solution.java
+++ ads1-assignments/m9/9.1 Merge Sort Improvements/Solution.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Merge m = new Merge();
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Merge m = new Merge();
         Scanner s = new Scanner(System.in);
         final int len = 50;
         String[] tokens = new String[len];
@@ -25,5 +25,5 @@ final class Solution {
             System.out.println(m.print(tokens));
             System.out.println();
         }
-	}
+    }
 }
