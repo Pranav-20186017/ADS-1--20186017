7b031ef32ae5c54b762d83a0740f7b865f2bbac1
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-04 20:05:32 +0530

    M10 A8 Commit

--- ads1-assignments/m10/Activity 8/Solution.java
+++ ads1-assignments/m10/Activity 8/Solution.java
@@ -1,23 +1,47 @@
 import java.util.Scanner;
+/**
+ * Class for star string.
+ */
 class StarString {
+	/**
+	 * generate the string with *'s.
+	 *
+	 * @param      st    { of type String}
+	 *
+	 * @return     { modified string with stars}
+	 */
 	public String generateStarString(String st) {
 		String result = "";
 		if (st == null) {
 			return st;
 		}
-		result = st.substring(0,1);
+		result = st.substring(0, 1);
 		if (st.trim().length() == 1) {
 			return result;
 		}
-		if (result.equals(st.substring(1,2))) {
+		if (result.equals(st.substring(1, 2))) {
 			result += "*";
 		}
 		st = st.substring(1);
 		return result + generateStarString(st);
 	}
 }
-class Solution {
-	public static void main(String[] args) {
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//unused.
+	}
+	/**
+	 * main fucntion.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		String line = s.nextLine();
 		StarString starstring = new StarString();
