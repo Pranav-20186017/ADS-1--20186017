cb8c500184058f355be3c693cf79d84a1c187314
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-04 19:51:28 +0530

    M10 A7 Commit

--- ads1-assignments/m10/Activity 7/Solution.java
+++ ads1-assignments/m10/Activity 7/Solution.java
@@ -3,18 +3,36 @@ import java.util.Scanner;
  * Class for factorial.
  */
 class Factorial {
-	public long getfactorial(long n) {
-		if (n == 0) {
-			return 1;
-		}
-		return n * getfactorial(n -1);
-	}
+    /**
+     * computes the factorial recursively.
+     *
+     * @param      n     { of type long}
+     *
+     * @return     { factorial of n }
+     * 
+     * Time Complexity - O(N).
+     */
+    public long getfactorial(long n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * getfactorial(n -1);
+    }
 }
-class Solution {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		long n = Long.parseLong(s.nextLine());
-		Factorial factorial = new Factorial();
-		System.out.println(factorial.getfactorial(n));
-	}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        long n = Long.parseLong(s.nextLine());
+        Factorial factorial = new Factorial();
+        System.out.println(factorial.getfactorial(n));
+    }
 }
