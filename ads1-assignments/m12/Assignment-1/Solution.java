e37f8e2f35e16dd2c788655a0c488ec314254104
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-06 12:56:10 +0530

    "ads1_m12_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m12/Assignment-1/Solution.java
+++ ads1-assignments/m12/Assignment-1/Solution.java
@@ -1,122 +1,145 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.ArrayList;
-public class Solution {
-	static ArrayList<Student> students = new ArrayList<>();
-	static ArrayList<Student> vaclist = new ArrayList<>();
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int Lines = Integer.parseInt(scanner.nextLine());
-		int vacancies = Integer.parseInt(scanner.nextLine());
-		int unres = Integer.parseInt(scanner.nextLine());
-		int bc = Integer.parseInt(scanner.nextLine());
-		int sc = Integer.parseInt(scanner.nextLine());
-		int st = Integer.parseInt(scanner.nextLine());
-		while (scanner.hasNext()) {
-			String[] tokens = scanner.nextLine().split(",");
-			Student s = new Student(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6]);
-			students.add(s);
-		}
-		selectionSort();
-		for (int i = 0; i < students.size(); i++) {
-			System.out.println(students.get(i).name + "," + students.get(i).total + "," + students.get(i).rescat);
-		}
-		System.out.println();
+/**
+ * Class for solution.
+ */
+final class Solution {
+    static ArrayList<Student> vaclist = new ArrayList<>();
+    static ArrayList<Student> students = new ArrayList<>();
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int Lines = Integer.parseInt(scanner.nextLine());
+        int vacancies = Integer.parseInt(scanner.nextLine());
+        int unres = Integer.parseInt(scanner.nextLine());
+        int bc = Integer.parseInt(scanner.nextLine());
+        int sc = Integer.parseInt(scanner.nextLine());
+        int st = Integer.parseInt(scanner.nextLine());
+        while (scanner.hasNext()) {
+            String[] tokens = scanner.nextLine().split(",");
+            Student s = new Student(tokens[0], tokens[1],
+                tokens[2], tokens[3], tokens[4],
+                tokens[5], tokens[6]);
+            students.add(s);
+        }
+        selectionSort();
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).name
+                + "," + students.get(i).total
+                + "," + students.get(i).rescat);
+        }
+        System.out.println();
+        meritFill(vacancies, unres, bc, sc, st);
+    }
+    public static void meritFill(final int vacancies,
+        final int unres, final int bc, final int sc, final int st) {
+        int ucount = 0;
+        int bcount = 0;
+        int ccount = 0;
+        int tcount = 0;
+        int vcount = 0;
+        for (int i = 0; i < students.size(); i++) {
+            if(ucount == unres) {
+                break;
+            } 
+                vaclist.add(students.get(i));
+                ucount++;
+                vcount++;
+        }
+        for (int i = 0; i < students.size(); i++) {
+            if(bcount == bc) {
+                break;
+            }
+            if(students.get(i).rescat.equals("BC")) {
+                if(!vaclist.contains(students.get(i))) {
+                    vaclist.add(students.get(i));
+                    bcount++;
+                    vcount++;
+                }   
+            }
+        }
+        for (int i = 0; i < students.size(); i++) {
+            if(ccount == sc) {
+                break;
+            }
+            if(students.get(i).rescat.equals("SC")) {
+                if(!vaclist.contains(students.get(i))) {
 
-		meritFill(vacancies, unres, bc, sc, st);
-
-	}
-	public static void meritFill(int vacancies, int unres, int bc, int sc, int st) {
-		int ucount = 0;
-		int bcount = 0;
-		int ccount = 0;
-		int tcount = 0;
-		int vcount = 0;
-		for (int i = 0; i < students.size(); i++) {
-			if(ucount == unres) break;
-				vaclist.add(students.get(i));
-				ucount++;
-				vcount++;
-			
-		}
-		for (int i = 0; i < students.size(); i++) {
-			if(bcount == bc) break;
-			if(students.get(i).rescat.equals("BC")) {
-				if(!vaclist.contains(students.get(i))) {
-					vaclist.add(students.get(i));
-					bcount++;
-					vcount++;
-				}
-				
-			}
-		}
-		for (int i = 0; i < students.size(); i++) {
-			if(ccount == sc) break;
-			if(students.get(i).rescat.equals("SC")) {
-				if(!vaclist.contains(students.get(i))) {
-
-					vaclist.add(students.get(i));
-					ccount++;
-					vcount++;
-				}
-			}	
-		}
-		for (int i = 0; i < students.size(); i++) {
-			if(tcount == st) break;
-			if(students.get(i).rescat.equals("ST")) {
-				if(!vaclist.contains(students.get(i))) {
-					vaclist.add(students.get(i));
-					tcount++;
-					vcount++;
-				}
-			}
-		}
-		for (int i = 0; i < students.size(); i++) {
-			if(vcount == vacancies) break;
-			if(!vaclist.contains(students.get(i))) {
-				vaclist.add(students.get(i));
-				vcount++;
-			}
-		}
-		sortfinal();
-		for (int i = 0; i < vaclist.size(); i++) {
-			if( i == vacancies) break;
-			System.out.println(vaclist.get(i).name + "," + vaclist.get(i).total + "," + vaclist.get(i).rescat);
-		}
-	}
-	public static void sortfinal() {
-		for (int i = vaclist.size() - 1; i >= 0; i--) {
-			Student max = vaclist.get(i);
-			int index = -1;
-			for (int j = i - 1; j >= 0; j--) {
-				if (max.compareTo(vaclist.get(j)) == 1) {
-					max = vaclist.get(j);
-					index = j;
-				}
-			}
-			Student temp = vaclist.get(i);
-			vaclist.set(i, max);
-			if (index != -1) {
-				vaclist.set(index, temp);
-			}
-		}
-	}
-	public static void selectionSort() {
-		for (int i = students.size() - 1; i >= 0; i--) {
-			Student max = students.get(i);
-			int index = -1;
-			for (int j = i - 1; j >= 0; j--) {
-				if (max.compareTo(students.get(j)) == 1) {
-					max = students.get(j);
-					index = j;
-				}
-			}
-			Student temp = students.get(i);
-			students.set(i, max);
-			if (index != -1) {
-				students.set(index, temp);
-			}
-		}
-	}
+                    vaclist.add(students.get(i));
+                    ccount++;
+                    vcount++;
+                }
+            }   
+        }
+        for (int i = 0; i < students.size(); i++) {
+            if(tcount == st) {
+                break;
+            }
+            if(students.get(i).rescat.equals("ST")) {
+                if(!vaclist.contains(students.get(i))) {
+                    vaclist.add(students.get(i));
+                    tcount++;
+                    vcount++;
+                }
+            }
+        }
+        for (int i = 0; i < students.size(); i++) {
+            if(vcount == vacancies) {
+                break;
+            } 
+            if(!vaclist.contains(students.get(i))) {
+                vaclist.add(students.get(i));
+                vcount++;
+            }
+        }
+        sortfinal();
+        for (int i = 0; i < vaclist.size(); i++) {
+            if( i == vacancies) {
+                break;
+            }
+            System.out.println(vaclist.get(i).name
+                + "," + vaclist.get(i).total + ","
+                + vaclist.get(i).rescat);
+        }
+    }
+    public static void sortfinal() {
+        for (int i = vaclist.size() - 1; i >= 0; i--) {
+            Student max = vaclist.get(i);
+            int index = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (max.compareTo(vaclist.get(j)) == 1) {
+                    max = vaclist.get(j);
+                    index = j;
+                }
+            }
+            Student temp = vaclist.get(i);
+            vaclist.set(i, max);
+            if (index != -1) {
+                vaclist.set(index, temp);
+            }
+        }
+    }
+    public static void selectionSort() {
+        for (int i = students.size() - 1; i >= 0; i--) {
+            Student max = students.get(i);
+            int index = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (max.compareTo(students.get(j)) == 1) {
+                    max = students.get(j);
+                    index = j;
+                }
+            }
+            Student temp = students.get(i);
+            students.set(i, max);
+            if (index != -1) {
+                students.set(index, temp);
+            }
+        }
+    }
 
 }
