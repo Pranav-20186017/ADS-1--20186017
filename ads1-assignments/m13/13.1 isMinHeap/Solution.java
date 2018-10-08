36483f7579b441fc58c7a19abd2ed5a3bf8d6e1c
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-08 10:59:25 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ads1-assignments/m13/13.1 isMinHeap/Solution.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String type = s.nextLine();
+		System.out.println(type);
+	}
+}
