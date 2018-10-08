7c03c69a1961a11c01c143d2c7e17cad6a3ae6b2
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-08 20:56:48 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m13/13.1 isMinHeap/Solution.java
+++ ads1-assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,30 +1,97 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-class minpq <E> {
-	ArrayList<E> list = new ArrayList<E>(); 
-	minpq() {
-
-	}
-
+import java.util.Arrays;
+class MinHeap<Gen extends Comparable<Gen>> {
+    /**
+     * Genric array type.
+     */
+    private Gen[] elements;
+    /**
+     * size of array.
+     */
+    private int size;
+    /**
+     * Constructs the object.
+     *
+     * @param      array  The array
+     */
+    MinHeap(final Gen[] array) {
+        elements = (Gen[]) array;
+        size = elements.length;
+    }
+    /**
+     * Determines if the array forms a minimum heap.
+     * Time Complexity :
+     * Worst : O(N)
+     * Average : O(N)
+     * Best : O(1)
+     *
+     * @return     True if minimum heap, False otherwise.
+     */
+    public boolean isMinHeap() {
+        int flag = 1;
+        if (size == 1) {
+            return true;
+        }
+        for (int i = size - 1; i > 0; i--) {
+            int halveVal = ((i + 1) / 2) - 1;
+            if (elements[i].compareTo(elements[halveVal]) < 0) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 0) {
+            return false;
+        }
+        return true;
+    }
 }
 class Solution {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		String type = s.nextLine();
-		System.out.println(type);
-		if (type.equals("String")) {
-			minpq<String> m = new minpq<String>();
-		}
-		if (type.equals("Integer")) {
-			minpq<Integer> m = new minpq<Integer>();
-		}
-		if (type.equals("Double")) {
-			minpq<Double> m = new minpq<Double>();
-		}
-		if (type.equals("Float")) {
-			minpq<Float> m = new minpq<Float>();
-		}
-		int cases = Integer.parseInt(s.nextLine());
-		System.out.println(cases);
-	}
+ public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String type = scan.nextLine();
+        int testcases = Integer.parseInt(scan.nextLine());
+        while (testcases > 0) {
+            switch (type) {
+            case "Integer" :
+                String[] stringArr = scan.nextLine().split(",");
+                Integer[] elements = Arrays.asList(stringArr)
+                                     .stream().map(Integer::valueOf)
+                                     .toArray(Integer[]::new);
+                MinHeap<Integer> minHeap = new MinHeap<>(elements);
+                boolean isMinHeap = minHeap.isMinHeap();
+                System.out.println(isMinHeap);
+                break;
+            case "String" :
+                stringArr = scan.nextLine().split(",");
+                MinHeap<String> minHeapString = new MinHeap<>(stringArr);
+                isMinHeap = minHeapString.isMinHeap();
+                System.out.println(isMinHeap);
+                break;
+            case "Float" :
+                stringArr = scan.nextLine().split(",");
+                if (stringArr[0].equals("")) {
+                    System.out.println("false");
+                    break;
+                }
+                Float[] elementsF = Arrays.asList(stringArr)
+                                    .stream().map(Float::valueOf)
+                                    .toArray(Float[]::new);
+                MinHeap<Float> minHeapFloat = new MinHeap<>(elementsF);
+                isMinHeap = minHeapFloat.isMinHeap();
+                System.out.println(isMinHeap);
+                break;
+            case "Double" :
+                stringArr = scan.nextLine().split(",");
+                Double[] elementsD = Arrays.asList(stringArr)
+                                     .stream().map(Double::valueOf)
+                                     .toArray(Double[]::new);
+                MinHeap<Double> minHeapDouble = new MinHeap<>(elementsD);
+                isMinHeap = minHeapDouble.isMinHeap();
+                System.out.println(isMinHeap);
+                break;
+            default:
+            }
+            testcases--;
+        }
+    }
 }
