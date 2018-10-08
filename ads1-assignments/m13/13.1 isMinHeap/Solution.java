04d84adab43c016c8c73cdd4d083881938a47a6d
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-08 11:14:11 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m13/13.1 isMinHeap/Solution.java
+++ ads1-assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,8 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+class minpq <E> {
+	ArrayList<E> list = new ArrayList<E>(); 
+	minpq() {
+
+	}
+
+}
 class Solution {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String type = s.nextLine();
 		System.out.println(type);
+		if (type.equals("String")) {
+			minpq<String> m = new minpq<String>();
+		}
+		if (type.equals("Integer")) {
+			minpq<Integer> m = new minpq<Integer>();
+		}
+		if (type.equals("Double")) {
+			minpq<Double> m = new minpq<Double>();
+		}
+		if (type.equals("Float")) {
+			minpq<Float> m = new minpq<Float>();
+		}
+		int cases = Integer.parseInt(s.nextLine());
+		System.out.println(cases);
 	}
 }
