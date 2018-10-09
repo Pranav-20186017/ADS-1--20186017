3c828323754bc5debfcce1bd02b5ccab08db1c2a
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-09 20:14:38 +0530

    "ads1_m14_a1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -2/10 "

--- ads1-assignments/m14/14.1 Dynamic Median Finding/Solution.py
+++ ads1-assignments/m14/14.1 Dynamic Median Finding/Solution.py
@@ -1,18 +1,13 @@
-def main():
-	n = int(input())
-	a = []
-	for i in range(n):
-		val = int(input())
-		a.append(val)
-		a.sort()
-		l = len(a)
-		if l % 2 != 0:
-			index = ((l+1) // 2) -1
-			print(float(a[index]))
-		if l % 2 == 0:
-			i1 = ((l+1) // 2) - 1
-			i2 = ((l+2) // 2) - 1
-			med = (a[i1] + a[i2]) / 2
-			print(float(med))
-if __name__ == "__main__":
-	main()
+n = int(input())
+ar = []
+for i in range(n):
+	ar.append(int(input()))
+	ar.sort()
+	l = len(ar) - 1
+	if(int(l % 2) != 0):
+		t = (l - 1) // 2
+		med = (ar[t] + ar[t + 1]) / 2
+		print(med)
+	else:
+		med = ar[(l + 1) // 2]
+		print(float(med))
