6c7d434f0f459959f1fa4f0f00339948d998fec5
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-09 22:42:16 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ads1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
-public class Solution {
-
+/**
+ * Class for solution.
+ */
+final class Solution {
     private Solution () {
-
+    	//unused.
     }
-
-    public static void main(String[] args) {
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-
         MinPQ<Float> minpq = new MinPQ<Float>(n);
         MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
-
         Float median = 0.0f;
         for (int i = 0; i < n; i++) {
             Float val = scan.nextFloat();
@@ -20,32 +24,25 @@ public class Solution {
             } else {
                 maxpq.insert(val);
             }
-
             if (minpq.size() - maxpq.size() > 1) {
                 maxpq.insert(minpq.delMin());
-
             }
             if (maxpq.size() - minpq.size() > 1) {
                 minpq.insert(maxpq.delMax());
                 
             }
-
             if (minpq.size() == maxpq.size()) {
                 median = (minpq.min() + maxpq.max()) / 2;
                 System.out.println(median);
             }
-
             if (maxpq.size() > minpq.size()) {
                 median = maxpq.max();
                 System.out.println(median);
             }
-
             if (minpq.size() > maxpq.size()) {
                 median = minpq.min();
                 System.out.println(median);
             }
-
         }
-
     }
 }
