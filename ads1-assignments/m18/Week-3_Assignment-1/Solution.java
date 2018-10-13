85b5b78aad758dae44a1a30a08baf06982b9fee0
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-13 11:48:26 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m18/Week-3_Assignment-1/Solution.java
+++ ads1-assignments/m18/Week-3_Assignment-1/Solution.java
@@ -1,22 +1,68 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-public class Solution {
+class Stock implements Comparable <Stock> {
+	private String ticker;
+	private float change;
+	Stock(String name, float percent) {
+		this.ticker = name;
+		this.change = percent;
+	}
+	public String getName() {
+		return this.ticker;
+	}
+	public float getChange() {
+		return this.change;
+	}
+	public int compareTo(Stock other) {
+		if (this.change > other.change) {
+			return 1;
+		}
+		if (this.change < other.change) {
+			return -1;
+		}
+		if (this.ticker.compareTo(other.ticker) > 0) {
+			return 1;
+		}
+		if (this.ticker.compareTo(other.ticker) < 0) {
+			return -1;
+		}
+		return 0;
+	}
+	public String toString() {
+		return this.ticker + " " + this.change;
+	}
+}
+class Solution {
+	private Solution() {
+		//unused.
+	}
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int n = Integer.parseInt(scan.nextLine());
-		while(n > 0) {
-			int hours = 6;
-			while(hours > 0) {
-				String[] line = scan.nextLine().split(",");
-				System.out.println(line[0] + " - " +line[1]);
-				
-				hours--;
+		int num = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < 6; i++) {
+			int count = 0;
+			MinPQ<Stock> minpq = new MinPQ<>();
+			MaxPQ<Stock> maxpq = new MaxPQ<>();
+			while (count < num) {
+				String[] tokens = scan.nextLine().split(",");
+				Stock stock = new Stock(tokens[0], Float.parseFloat(tokens[1]));
+				minpq.insert(stock);
+				maxpq.insert(stock);
+				count++;
+			}
+			BinarySearchTree<String, Float> stockbest = new  BinarySearchTree<>();
+			BinarySearchTree<String, Float> stockworst = new BinarySearchTree<>();
+			for (int j = 0; j < 5; j++) {
+				Stock maxpqbest = maxpq.delMax();
+				System.out.println(maxpqbest);
+				stockbest.put(maxpqbest.getName(), maxpqbest.getChange());
 			}
 			System.out.println();
-			n--;
+			for(int k = 0; k < 5; k++) {
+				Stock minpqworst = minpq.delMin();
+				System.out.println(minpqworst);
+				stockworst.put(minpqworst.getName(), minpqworst.getChange());
+			}
+			System.out.println();
 		}
 	}
 }
