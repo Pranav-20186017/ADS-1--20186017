da91931652cb241583236f1eff97aafdb9e91040
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-22 21:01:24 +0530

    "ads1_m19_a1: 0/0 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m19/19.1 BST/Solution.java
+++ ads1-assignments/m19/19.1 BST/Solution.java
@@ -1,42 +1,58 @@
 import java.util.Scanner;
-import java.util.Arrays;
 /**
  * Class for solution.
  */
-class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused.
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-        while(sc.hasNext()) {
+final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree bst = new BinarySearchTree();
+        while (sc.hasNext()) {
             String input = sc.nextLine();
             String[] tokens = input.split(",");
             switch (tokens[0]) {
-                case "put": Arrays.toString(tokens); 
-                            break;
-                case "get": Arrays.toString(tokens);
-                            break;
-                case "floor": Arrays.toString(tokens); 
-                              break;
-                case "ceiling": Arrays.toString(tokens);
-                                break;
-                case "min":     Arrays.toString(tokens);
-                                break;
-                case "max":     Arrays.toString(tokens);
-                                break;
-                case "select":  Arrays.toString(tokens);
-                                break;
+                case "put":
+                    bst.put(new BookList(tokens[1], tokens[2],
+                        Float.parseFloat(tokens[2 + 1])),
+                        Integer.parseInt(tokens[2 + 2]));
+                break;
+                case "get":
+                    System.out.println(bst.get(new BookList(tokens[1],
+                        tokens[2], Float.parseFloat(tokens[2 + 1]))));
+                break;
+                case "min":
+                    System.out.println(bst.min());
+                break;
+                case "max":
+                    System.out.println(bst.max());
+                break;
+                case "floor":
+                    System.out.println(bst.floor(new BookList(tokens[1],
+                        tokens[2], Float.parseFloat(tokens[2 + 1]))));
+                break;
+                case "ceiling":
+                    System.out.println(bst.ceiling(new BookList(tokens[1],
+                        tokens[2], Float.parseFloat(tokens[2 + 1]))));
+                break;
+                case "select":
+                    System.out.println(bst.select(Integer.parseInt(
+                        tokens[1])));
+                break;
+                default:
+                break;
             }
         }
-	}
+    }
 }
