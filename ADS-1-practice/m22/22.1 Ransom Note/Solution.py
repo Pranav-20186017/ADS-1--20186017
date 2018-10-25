6b61e4f539b9c84963c99cee32aff327330cc9af
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-25 10:40:20 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 10/10 "

--- ADS-1-practice/m22/22.1 Ransom Note/Solution.py
+++ ADS-1-practice/m22/22.1 Ransom Note/Solution.py
@@ -1,16 +1,20 @@
-length = input().split(" ")
-length = list(map(int, length))
-mag_length = length[0]
-note_length = length[1]
-mag = input().split(" ")
-ransom = input().split(" ")
-counter = 0
-for i in ransom:
-    if i in mag:
-        temp = mag.index(i)
-        del mag[temp]
-        counter = counter + 1
-if counter == note_length:
+"""
+Author: Pranav Surampudi
+Date: 25/10/2018
+"""
+LENGTH = input().split(" ")
+LENGTH = list(map(int, LENGTH))
+MAGL = LENGTH[0]
+NOTE = LENGTH[1]
+MAG = input().split(" ")
+RANSOM = input().split(" ")
+COUNTER = 0
+for i in RANSOM:
+    if i in MAG:
+        temp = MAG.index(i)
+        del MAG[temp]
+        COUNTER = COUNTER + 1
+if COUNTER == NOTE:
     print("Yes")
 else:
     print("No")
