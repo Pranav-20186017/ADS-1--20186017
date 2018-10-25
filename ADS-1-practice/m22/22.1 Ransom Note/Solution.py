58132c5312b348c18cda0fef18e9bdc42329cc71
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-25 10:32:37 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 4/10 "

--- ADS-1-practice/m22/22.1 Ransom Note/Solution.py
+++ ADS-1-practice/m22/22.1 Ransom Note/Solution.py
@@ -6,11 +6,11 @@ mag = input().split(" ")
 ransom = input().split(" ")
 counter = 0
 for i in ransom:
-	if i in mag:
-		temp = mag.index(i)
-		del mag[temp]
-		counter = counter + 1
+    if i in mag:
+        temp = mag.index(i)
+        del mag[temp]
+        counter = counter + 1
 if counter == note_length:
-	print("Yes")
+    print("Yes")
 else:
-	print("No")
+    print("No")
