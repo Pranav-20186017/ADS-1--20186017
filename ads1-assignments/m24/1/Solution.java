3f4fdc0cdf45b38d044c16271f681ff4ebee5eba
Pranav Surampudi <sharmapranav307@msitprogram.net>
2018-10-27 11:14:41 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m24/1/Solution.java
+++ ads1-assignments/m24/1/Solution.java
@@ -1,55 +1,81 @@
 import java.util.Scanner;
+/**
+ * Class for student.
+ */
 class Student {
-	private String studentname;
-	private double totalmarks;
-	Student(String name, double marks) {
-		this.studentname = name;
-		this.totalmarks = marks;
-	}
-	public String getname() {
-		return this.studentname;
-	}
-	public double getmarks() {
-		return this.totalmarks;
-	}
-	public String toString() {
-		String str = "";
-		str = str + this.studentname + this.totalmarks;
-		return str;
-	}
+    /**
+     * variable for student name.
+     */
+    private String studentname;
+    /**
+     * varibale for student marks.
+     */
+    private double totalmarks;
+    /**
+     * Constructs the object.
+     *
+     * @param      name   The name
+     * @param      marks  The marks
+     */
+    Student(String name, double marks) {
+        this.studentname = name;
+        this.totalmarks = marks;
+    }
+    public String getname() {
+        return this.studentname;
+    }
+    public double getmarks() {
+        return this.totalmarks;
+    }
+    public String toString() {
+        String str = "";
+        str = str + this.studentname + this.totalmarks;
+        return str;
+    }
 }
-class Solution {
-	private Solution() {
-		//unused
-	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int putnum = Integer.parseInt(scan.nextLine());
-		SeparateChainingHashST<Integer, Student> schsobj = new SeparateChainingHashST<>();
-		for (int i = 0; i < putnum; i++) {
-			String[] tokens = scan.nextLine().split(",");
-			Student studentobj = new Student(tokens[1], Double.parseDouble(tokens[2]));
-			schsobj.put(Integer.parseInt(tokens[0]), studentobj);
-		}
-		int querynum = Integer.parseInt(scan.nextLine());
-		for (int j = 0; j < querynum; j++) {
-			String[] querytokens = scan.nextLine().split(" ");
-			if (querytokens[0].equals("get")) {
-				if (Integer.parseInt(querytokens[2]) == 1) {
-					if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
-						System.out.println("Student doesn't exists...");
-					} else {
-						System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getname());
-					}
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int putnumber = Integer.parseInt(scan.nextLine());
+        SeparateChainingHashST<Integer, Student> chainobj = new SeparateChainingHashST<>();
+        for (int i = 0; i < putnumber; i++) {
+            String[] tokens = scan.nextLine().split(",");
+            Student stobj = new Student(tokens[1], Double.parseDouble(tokens[2]));
+            chainobj.put(Integer.parseInt(tokens[0]), stobj);
+        }
+        int querynum = Integer.parseInt(scan.nextLine());
+        for (int j = 0; j < querynum; j++) {
+            String[] query = scan.nextLine().split(" ");
+            if (query[0].equals("get")) {
+                if (Integer.parseInt(query[2]) == 1) {
+                    if (chainobj.get(Integer.parseInt(query[1])) == null) {
+                        System.out.println("Student doesn't exists...");
+                    } else {
+                        System.out.println(chainobj.get(Integer.parseInt(query[1])).getname());
+                    }
 
-				} else {
-					if (schsobj.get(Integer.parseInt(querytokens[1])) == null) {
-						System.out.println("Student doesn't exists...");
-					} else {
-						System.out.println(schsobj.get(Integer.parseInt(querytokens[1])).getmarks());
-					}
-				}
-			}
-		}
-	}
+                } else {
+                    if (chainobj.get(Integer.parseInt(query[1])) == null) {
+                        System.out.println("Student doesn't exists...");
+                    } else {
+                        System.out.println(chainobj.get(Integer.parseInt(query[1])).getmarks());
+                    }
+                }
+            }
+        }
+    }
 }
